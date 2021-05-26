461bbc611e975685bd3f7994959eb3397e235da0
FeroHriadel <ferdinand.hriadel@gmail.com>
2021-05-26 20:38:08 +0200

    add comment + image

--- routes/cloudinaryRoutes.js
+++ routes/cloudinaryRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { loggedInOnly, adminOnly } = require('../middleware/authMiddleware');
 const { uploadImages, removeImage } = require("../controllers/cloudinaryControllers");
 
-router.post("/uploadimages", loggedInOnly, adminOnly, uploadImages);
+router.post("/uploadimages", loggedInOnly, uploadImages);
 router.delete("/removeimage/:public_id/", loggedInOnly, adminOnly, removeImage);
+router.delete("/removecommentimage/:public_id/", removeImage); //same as removeimage above but no authorization => for comment images
 
 module.exports = router;
